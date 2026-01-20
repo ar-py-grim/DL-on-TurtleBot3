606cd417eafcac173d93f4a69901995899056ac7
Arpit <123756415+ar-py-grim@users.noreply.github.com>
2026-01-20 16:44:57 -0500

    Update setup.py

--- src/turtlebot3_dqn/setup.py
+++ src/turtlebot3_dqn/setup.py
@@ -13,8 +13,6 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob.glob('launch/*.py')),
-        # ('share/' + package_name + '/model', glob.glob(os.path.join('model', '*.h5'))),
-        # ('share/' + package_name + '/model', glob.glob(os.path.join('model', '*.json'))),
     ],
     
     install_requires=['setuptools','launch'],
